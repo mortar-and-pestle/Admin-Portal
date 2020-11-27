78180e9d320462852a4a526102068a44a4e67832
isaac <isaacderxc@gmail.com>
2020-11-26 19:55:19 -0600

    small changes

--- admin/forms.py
+++ admin/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField
-class adminSelector(FlaskForm):
-    adminType = SelectField('types: ', choices = [('Support', 'ADMIN'), ('Finance', 'FINANCE_ADMIN'),
-                            ('Sales', 'SALES_ADMIN'), ('HR', 'HR_ADMIN'), ('Technology', 'TECH_ADMIN'),('User', 'REGULAR_USER')])
-    submit =  SubmitField("submit selection")
+from wtforms import SubmitField, SelectField, StringField, PasswordField, BooleanField
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
